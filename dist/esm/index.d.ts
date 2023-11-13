23a803583c43bce9aeaf716df3cf060169a4dd05
Bebe Jane <bjornthief@gmail.com>
2023-11-13 13:20:18 +0100

    updates

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,1 +1,7 @@
-export { default } from './DraftMode';
+export type DraftModeProps = {
+    enabled: boolean;
+    draftUrl?: string;
+    tag?: string;
+    path?: string;
+};
+export default function DraftMode({ enabled, draftUrl, tag, path }: DraftModeProps): import("react/jsx-runtime").JSX.Element | null;
