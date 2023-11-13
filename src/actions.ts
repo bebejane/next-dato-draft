d275c3c2036f91837e05cf2b9d06ba69f2e31a8a
Bebe Jane <bjornthief@gmail.com>
2023-11-13 13:36:15 +0100

    updates

--- src/actions.ts
+++ src/actions.ts
@@ -1,3 +1,4 @@
+import { cache } from 'react';
 import { draftMode } from 'next/headers'
 import { redirect } from 'next/navigation'
 import { revalidateTag as rt, revalidatePath as rp } from 'next/cache'
